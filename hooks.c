fce4195ae7958507e5c5092c8effa99ee9011e98
173210 <root.3.173210@live.com>
2015-03-10 11:44:07 +0900

    Add static qualifier for _sceUsbbdReqRecvCB

--- hooks.c
+++ hooks.c
@@ -11,7 +11,7 @@ typedef struct {
 	const char *name;
 } name_t;
 
-int (* _sceUsbbdReqRecvCB)(struct UsbdDeviceReq *, void *, int);
+static int (* _sceUsbbdReqRecvCB)(struct UsbdDeviceReq *, void *, int);
 
 static const char *getNameOfbDecriptorType(unsigned bDecriptorType)
 {
